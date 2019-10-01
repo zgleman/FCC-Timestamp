2e67f034782ada6d805ee0c0d2458aa62d4dddfc
Glitch (zglemantimestamp) <none>
2019-10-01 11:55:36 +0000

    ✋🏫 Checkpoint ./server.js:7037687/101

--- server.js
+++ server.js
@@ -24,6 +24,9 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("https://zglemantimestamp.glitch.me/api/timestamp/:date_string?", function(req, res){
+  
+});
 
 
 // listen for requests :)
